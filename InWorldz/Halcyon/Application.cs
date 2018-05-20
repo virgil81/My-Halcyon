dbd86c1ce4834131888ae074660fff6916032781
EmperorStarfinder <emperor@secondgalaxy.com>
2018-05-20 00:39:23 -0400

    Make the default crash directory a constant
    - Make the default crash directory a constant
    - General cleaning

--- InWorldz/Halcyon/Application.cs
+++ InWorldz/Halcyon/Application.cs
@@ -1,29 +1,29 @@
-/*
- * Copyright (c) InWorldz Halcyon Developers
- * Copyright (c) Contributors, http://opensimulator.org/
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the OpenSimulator Project nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/// <summary>
+///     Copyright (c) InWorldz Halcyon Developers
+///     Copyright (c) Contributors, http://opensimulator.org/
+/// 
+///     Redistribution and use in source and binary forms, with or without
+///     modification, are permitted provided that the following conditions are met:
+///         * Redistributions of source code must retain the above copyright
+///         notice, this list of conditions and the following disclaimer.
+///         * Redistributions in binary form must reproduce the above copyright
+///         notice, this list of conditions and the following disclaimer in the
+///         documentation and/or other materials provided with the distribution.
+///         * Neither the name of the OpenSimulator Project nor the
+///         names of its contributors may be used to endorse or promote products
+///         derived from this software without specific prior written permission.
+/// 
+///     THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+///     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+///     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+///     DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+///     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+///     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+///     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+///     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+///     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+///     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// </summary>
 
 using System;
 using System.IO;
@@ -38,27 +38,27 @@ using OpenSim.Framework.Console;
 namespace OpenSim
 {
     /// <summary>
-    /// Starting class for the OpenSimulator Region
+    ///     Starting class for the OpenSimulator Region
     /// </summary>
     public static class Application
     {
         /// <summary>
-        /// Text Console Logger
+        ///     Text Console Logger
         /// </summary>
         private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         
         /// <summary>
-        /// Save Crashes in the bin/crashes folder.  Configurable with m_crashDir
+        ///     Save Crashes in the bin/crashes folder.  Configurable with m_crashDir
         /// </summary>
         public static bool m_saveCrashDumps = false;
 
         /// <summary>
-        /// Directory to save crash reports to.  Relative to bin/
+        ///     Directory to save crash reports to.  Relative to bin/
         /// </summary>
-        public static string m_crashDir = "crashes";
+        public static string m_crashDir = Constants.DEFAULT_CRASH_DIR;
 
         /// <summary>
-        /// Instance of the OpenSim class.  This could be OpenSim or OpenSimBackground depending on the configuration
+        ///     Instance of the OpenSim class.  This could be OpenSim or OpenSimBackground depending on the configuration
         /// </summary>
         private static OpenSimBase m_sim = null;
 
@@ -69,8 +69,7 @@ namespace OpenSim
             Environment.ExitCode = 1;
 
             // First line, hook the appdomain to the crash reporter
-            AppDomain.CurrentDomain.UnhandledException +=
-                new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
 
             ServicePointManager.DefaultConnectionLimit = 12;
 
@@ -81,30 +80,32 @@ namespace OpenSim
             var pidFile = new PIDFileManager(configSource.Configs["Startup"].GetString("pidfile", string.Empty));
 
             pidFile.SetStatus(PIDFileManager.Status.Starting);
+        
             // Configure Log4Net
             configSource.AddSwitch("Startup", "logconfig");
             string logConfigFile = configSource.Configs["Startup"].GetString("logconfig", String.Empty);
+
             if (!String.IsNullOrEmpty(logConfigFile))
             {
                 XmlConfigurator.Configure(new System.IO.FileInfo(logConfigFile));
-                m_log.InfoFormat("[HALCYON MAIN]: configured log4net using \"{0}\" as configuration file", 
-                                 logConfigFile);
+                m_log.InfoFormat("[My Halcyon Main]: configured log4net using \"{0}\" as configuration file", logConfigFile);
             } 
             else
             {
                 XmlConfigurator.Configure();
-                m_log.Info("[HALCYON MAIN]: configured log4net using default Halcyon.exe.config");
+                m_log.Info("[My Halcyon Main]: configured log4net using default Halcyon.exe.config");
             }
 
             m_log.Info("Performing compatibility checks... ");
             string supported = String.Empty;
+
             if (Util.IsEnvironmentSupported(ref supported))
             {
-                m_log.Info("Environment is compatible.\n");
+                m_log.Info("[My Halcyon Main]: Environment is compatible.\n");
             }
             else
             {
-                m_log.Warn("Environment is unsupported (" + supported + ")\n");
+                m_log.Warn("[My Halcyon Main]: Environment is unsupported (" + supported + ")\n");
             }
 
             // Configure nIni aliases and localles
@@ -153,6 +154,7 @@ namespace OpenSim
                 m_sim.Startup();
 
                 pidFile.SetStatus(PIDFileManager.Status.Running);
+
                 while (true)
                 {
                     try
@@ -171,7 +173,8 @@ namespace OpenSim
         private static bool _IsHandlingException = false; // Make sure we don't go recursive on ourself
 
         /// <summary>
-        /// Global exception handler -- all unhandlet exceptions end up here :)
+        ///     Global exception handler
+        ///     all unhandlet exceptions end up here :)
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -183,6 +186,7 @@ namespace OpenSim
             }
 
             _IsHandlingException = true;
+            
             // TODO: Add config option to allow users to turn off error reporting
             // TODO: Post error report (disabled for now)
 
@@ -193,6 +197,7 @@ namespace OpenSim
 
             msg += "Exception: " + e.ExceptionObject.ToString() + "\r\n";
             Exception ex = (Exception) e.ExceptionObject;
+
             if (ex.InnerException != null)
             {
                 msg += "InnerException: " + ex.InnerException.ToString() + "\r\n";
@@ -201,7 +206,7 @@ namespace OpenSim
             msg += "\r\n";
             msg += "Application is terminating: " + e.IsTerminating.ToString() + "\r\n";
 
-            m_log.ErrorFormat("[APPLICATION]: {0}", msg);
+            m_log.ErrorFormat("[Application]: {0}", msg);
 
             if (m_saveCrashDumps)
             {
@@ -212,7 +217,9 @@ namespace OpenSim
                     {
                         Directory.CreateDirectory(m_crashDir);
                     }
+
                     string log = Util.GetUniqueFilename(ex.GetType() + ".txt");
+
                     using (StreamWriter m_crashLog = new StreamWriter(Path.Combine(m_crashDir, log)))
                     {
                         m_crashLog.WriteLine(msg);
@@ -222,7 +229,7 @@ namespace OpenSim
                 }
                 catch (Exception e2)
                 {
-                    m_log.ErrorFormat("[CRASH LOGGER CRASHED]: {0}", e2);
+                    m_log.ErrorFormat("[Crash Logger Crashed]: {0}", e2);
                 }
             }
 
