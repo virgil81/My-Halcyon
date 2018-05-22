68b11f276ce6399129e689ed079bdd50c84c5def
EmperorStarfinder <emperor@secondgalaxy.com>
2018-05-22 09:42:47 -0400

    Rename InWorldz directory to Enhanced
    - Rename InWorldz directory to Enhanced
    - Fix links for InWorldz\ to be Enhanced\

---
+++ Enhanced/InWorldz.Arbiter/Definitions/generate.bat
@@ -0,0 +1,8 @@
+..\..\..\bin\flatc-win.exe --csharp Vector3.fbs
+..\..\..\bin\flatc-win.exe --csharp Quaternion.fbs
+..\..\..\bin\flatc-win.exe --csharp HalcyonPrimitiveBaseShape.fbs
+..\..\..\bin\flatc-win.exe --csharp HalcyonPrimitive.fbs
+..\..\..\bin\flatc-win.exe --csharp HalcyonGroup.fbs
+
+move /Y .\Enhanced\Arbiter\Serialization\* ..\Serialization
+rmdir /S/Q .\InWorldz
