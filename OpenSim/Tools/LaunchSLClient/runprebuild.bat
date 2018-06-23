f6e40380de167a412f6e62c4b802431c9f7add46
EmperorStarfinder <emperor@secondgalaxy.com>
2018-06-23 19:33:13 -0400

    Correctiong the directory paths in the csproj files
    - I was off by one folder in each directory path.  Build should now work.

--- OpenSim/Tools/LaunchSLClient/runprebuild.bat
+++ OpenSim/Tools/LaunchSLClient/runprebuild.bat
@@ -1,4 +1,4 @@
-..\..\Release\bin\Prebuild.exe /target nant
-..\..\Release\bin\Prebuild.exe /target vs2005
+..\..\..\Release\bin\Prebuild.exe /target nant
+..\..\..\Release\bin\Prebuild.exe /target vs2005
 
 echo C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\msbuild LaunchSLClient.sln > compile.bat
